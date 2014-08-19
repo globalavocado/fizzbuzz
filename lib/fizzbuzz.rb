c21a68d0312ff1df23dac1d2ec31b3e7024c7026
globalavocado <globalavocado@gmail.com>
2014-08-19 18:40:11 +0100

    added functionality for numbers divisible 15 to be replaced with 'fizzbuzz'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,16 +1,18 @@
 class Fixnum
 
-	# def is_divisible_by_15
-	# 	if self % 15 == 0
-	# 		true
-	# 	end
-	# end
-
 	def is_divisible_by_3
 		# uses modulo
 		if self % 3 == 0 
 			true
 		else 
+			false
+		end
+	end
+
+	def is_divisible_by_15
+		if self % 15 == 0
+			true
+		else
 			false
 		end
 	end
@@ -24,7 +26,9 @@ class Fixnum
 	end
 
 	def fizzbuzz
-		if self.is_divisible_by_3
+		if self.is_divisible_by_15
+			"fizzbuzz"
+		elsif self.is_divisible_by_3
 			"fizz"
 		elsif self.is_divisible_by_5
 			"buzz"
@@ -35,4 +39,6 @@ class Fixnum
 
 end
 
-(1..10).to_a.each{|number| puts number.fizzbuzz}
+
+
+(1..30).to_a.each{|number| puts number.fizzbuzz}
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,12 +10,20 @@ describe 'fizzbuzz' do
 		expect(5.is_divisible_by_5).to eq true
 	end
 
+	it "replaces numbers divisible by 15 with 'fizzbuzz'" do
+		expect(15.is_divisible_by_15).to eq true
+	end
+
 	it "knows if 2 divisible by 3" do
 		expect(2.is_divisible_by_3).to eq false
 	end
 
 	it "knows if 8 is divisible by 5" do
 		expect(8.is_divisible_by_5).to eq false
+	end
+
+	it "knows if 16 is divisible by 15" do
+		expect(16.is_divisible_by_15).to eq false
 	end
 
 	it "says fizz for 3" do
@@ -26,4 +34,7 @@ describe 'fizzbuzz' do
 		expect(5.fizzbuzz).to eq "buzz"
 	end
 
+	it "says fizzbuzz for 15" do
+		expect(15.fizzbuzz).to eq "fizzbuzz"
+	end
 end
